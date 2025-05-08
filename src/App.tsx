c52dc5e0b4cdc3a613d165937d1d9984fa441b9b
Fredrik Thorsson <fredrik.thorsson@scandichotels.com>
2025-05-08 14:04:27 +0200

    feat: add counter logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Counter from "./Components/Counter";
 
 function App() {
-  return <Counter />;
+  return <Counter sinceDate="2025-04-01" />;
 }
 
 export default App;
--- src/Components/Counter/index.tsx
+++ src/Components/Counter/index.tsx
@@ -1,6 +1,30 @@
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import { useEffect, useState } from "react";
 
-export default function Counter() {
-  const now = dayjs().get("year");
-  return <div>{now}</div>;
+dayjs.extend(duration);
+
+type CounterProps = {
+  sinceDate: string;
+};
+
+export default function Counter({ sinceDate }: CounterProps) {
+  const [timeSince, setTimeSince] = useState("");
+
+  useEffect(() => {
+    const update = () => {
+      const now = dayjs();
+      const start = dayjs(sinceDate);
+      const diff = dayjs.duration(now.diff(start));
+
+      const formatted = `${diff.months()}m ${diff.days()}d ${diff.hours()}h ${diff.minutes()}m ${diff.seconds()}s`;
+      setTimeSince(formatted);
+    };
+
+    update();
+    const interval = setInterval(update, 1000);
+    return () => clearInterval(interval);
+  }, [sinceDate]);
+
+  return <div>{`Time spent on the bench: ${timeSince}`}</div>;
 }
